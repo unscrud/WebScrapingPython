2a1f626ff2ded1763ead9d5b9db603cdd59595a2
Daniel Carvalho <daniel.devop@gmail.com>
2020-07-05 17:28:55 -0300

    Imports

--- webscraping.py
+++ webscraping.py
@@ -1,4 +1,13 @@
 # Tutorial do Canal Codigo Fonte do Youtube (https://youtu.be/Vxl5jUltHBo)
+
+import time
+import requests
+import pandas as pd
+from bs4 import BeaultifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import options
+import json
+
 # 1. Pegar o conteúdo HTML a partir da URL
 # 2. Parsear o conteúdo HTML - BeaultifulSoup
 # 3. Estruturar o conteúdo em um Data Frame - Pandas
