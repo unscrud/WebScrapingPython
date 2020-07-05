83c1beeb2e3534bc7b19fa1ca02ec186b47b3e91
Daniel Carvalho <daniel.devop@gmail.com>
2020-07-05 13:06:50 -0300

    Commit inicial

---
+++ webscraping.py
@@ -0,0 +1,6 @@
+# Tutorial do Canal Codigo Fonte do Youtube
+# 1. Pegar o conteúdo HTML a partir da URL
+# 2. Parsear o conteúdo HTML - BeaultifulSoup
+# 3. Estruturar o conteúdo em um Data Frame - Pandas
+# 4. Transformar os Dados em um Dicionário de dados próprio
+# 5. Converter e salvar em um arquivo JSON
