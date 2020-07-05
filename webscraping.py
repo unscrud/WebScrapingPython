3a9cc71370d037491ab8dc26f23ba250e4b2cc83
Daniel Carvalho <daniel.devop@gmail.com>
2020-07-05 17:25:09 -0300

    Libs

--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,13 @@
-# Tutorial do Canal Codigo Fonte do Youtube
+# Tutorial do Canal Codigo Fonte do Youtube (https://youtu.be/Vxl5jUltHBo)
 # 1. Pegar o conteúdo HTML a partir da URL
 # 2. Parsear o conteúdo HTML - BeaultifulSoup
 # 3. Estruturar o conteúdo em um Data Frame - Pandas
 # 4. Transformar os Dados em um Dicionário de dados próprio
 # 5. Converter e salvar em um arquivo JSON
+
+# Libs que serão utilizadas
+# pip install requests2
+# pip install pandas
+# pip install lxml
+# pip install beautifulsoup4
+# pip install selenium
